1e20114d2a6407bf4d763ea3773a64ced80aafdd
vnskingmotor <57930370+VnsKing001@users.noreply.github.com>
2021-03-11 20:37:57 +0700

    ok

---
+++ src/motorRideApp/component/motorRideAppImages.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export function MotorRideAppImagesContainer({src}) {
+    return (
+        <>
+            <div className="Ride-App-Moto-List">
+                <div className="moto_title">
+                    <h1>COMPATIBLE WITH</h1>
+                    <p>MV Agusta models equipped with second generation 5,5” TFT screens</p>
+                </div>
+                <div className="moto_list_content">
+                    {src.map((imgSrcData)=> (
+                        <div className="moto_list_item">
+                            <div className="moto_list_picture">
+                                <img 
+                                    className="moto_list_img" 
+                                    src={`/img/${imgSrcData.imgSrc}`}
+                                    alt=""></img>
+                            </div>
+                            <div className="moto_list_name">
+                                <strong>{imgSrcData.name}</strong>
+                            </div>
+                        </div>
+                    ))};
+                </div>
+            </div>
+        </>
+    );
+}
--- src/motorRideApp/motorRideApp_Layout.js
+++ src/motorRideApp/motorRideApp_Layout.js
@@ -1,32 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BodyContainer } from './component/bodyContainer';
-import { BodyContainer2 } from './component/BodyContainer-2';
-import { BodyContainer0 } from './component/BodyContainer-0';
-import { BodyContainer4 } from './component/BodyContainer-4';
 import NavDesktop from '../generalLayout/navDesk';
 import Nav from '../generalLayout/nav';
 import PageName from '../generalLayout/pageName';
+import { MotorRideAppPhone } from './component/motorRideAppPhone';
+import { MobileRideAppContainer } from './component/mobileRideApp';
+import { MotorRideAppHistoryContainer } from './component/motorRideAppHistory';
+import { MotorRideAppImagesContainer } from './component/motorRideAppImages';
+import { MotorRideDownloadContainer } from './component/motorRideDownload';
 
 
 const imgData = [
   {
     name: "BRUTALE 1000 SERIE ORO",
-    imgSrc: "brutale_pr.png",
+    imgSrc: "main.png",
   },
   {
     name: "BRUTALE 1000 RR",
-    imgSrc: "brutale_1000.png",
+    imgSrc: "main (1).png",
   },
   {
     name: "SUPERVELOCE 800 SERIE ORO",
-    imgSrc: "supervelocer_pr.png",
+    imgSrc: "main (2).png",
   },
   {
     name: "SUPERVELOCE 800",
-    imgSrc: "superveloce_pr.png",
+    imgSrc: "main (3).png",
   },
 ];
+
+const imgPhoneData = {
+  
+  imgSrc0: "app-preview-2.png",
+  
+  imgSrc1: "itineraries-xl.png",
+  
+  imgSrc2: "trips-m.png"
+  
+};
+
 export default function MotorRideAppLayout() {
 
     return (
@@ -34,10 +45,11 @@ export default function MotorRideAppLayout() {
           <NavDesktop colorBlack={true}/>
           <Nav color="white" />
           <PageName namePage="Motor Ride App" />
-          <BodyContainer0 />
-          <BodyContainer />
-          <BodyContainer2 imgSrc={imgData} />
-          <BodyContainer4 />
+          <MobileRideAppContainer/>
+          <MotorRideAppHistoryContainer/>
+          <MotorRideAppImagesContainer src={imgData}/>
+          <MotorRideAppPhone imgPhoneSrc={imgPhoneData}/>
+          <MotorRideDownloadContainer/>
       </>
     );
 }
